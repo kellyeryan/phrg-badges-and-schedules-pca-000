6127b2dcde9f3fb53fad205637ed8ed315c4bc47
kellyeryan <kellyeryan@gmail.com>
2019-07-15 16:43:02 -0400

    Finish badges and schedules

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(names)
+  names.map { |name| badge_maker(name) }
+end
+
+
+def assign_rooms(speakers)
+  speakers.each_with_index.map { |speaker, index| "Hello, #{speaker}! You'll be assigned to room #{index + 1}!" }
+end
+
+names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def printer(names)
+  batch_badge_creator(names).each do |name|
+    puts name
+  end
+  assign_rooms(names).each do |name|
+    puts name
+  end
+end
